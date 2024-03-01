999846d6feceaf0bfc58e54f2a9a318af51fe78f
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-03-01 22:47:58 +0000

    quiet conda install

--- gym/prepare.sh
+++ gym/prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 install_conda() {
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh
-    rm Miniconda3-latest-Linux-x86_64.sh
+    mkdir -p ~/miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm -rf ~/miniconda3/miniconda.sh
 }
 
 wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
