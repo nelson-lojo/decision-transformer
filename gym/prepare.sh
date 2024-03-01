5190a471aeab98644293953dcda4e8608e7103c1
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-03-01 22:42:45 +0000

    streamline deployment script

--- gym/prepare.sh
+++ gym/prepare.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
+tar -xf mujoco210-linux-x86_64.tar.gz 
+mkdir ~/.mujoco/
+mv mujoco210/ ~/.mujoco/mujoco210
+rm mujoco210-linux-x86_64.tar.gz 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin
+
+/opt/conda/bin/conda env create -f conda_env.yml
+conda init
+echo "conda activate decision-transformer-gym" > ~/.bashrc
+
 cd data/ && python download_d4rl_datasets.python
 cd ../
 
