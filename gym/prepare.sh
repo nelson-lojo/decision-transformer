7de3a98772f0baacc7d0fe2e67541bdf6bbee4d9
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-03-02 23:59:32 +0000

    it works on the rtx6000

--- gym/prepare.sh
+++ gym/prepare.sh
@@ -8,12 +8,15 @@ install_conda() {
     rm -rf ~/miniconda3/miniconda.sh
 }
 
-download_mujoco() {
+install_mujoco() {
     wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
     tar -xf mujoco210-linux-x86_64.tar.gz 
     mkdir ~/.mujoco/
     mv mujoco210/ ~/.mujoco/mujoco210
     rm mujoco210-linux-x86_64.tar.gz 
+
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 }
 
 prep_env() {
@@ -21,16 +24,13 @@ prep_env() {
 
     conda activate decision-transformer-gym
     $CONDA_PREFIX/bin/pip3 install git+https://github.com/Farama-Foundation/d4rl@master#egg=d4rl
+    $CONDA_PREFIX/bin/pip install typing_extensions==4.4.0
 }
 
 download_data() {
     sudo chmod 777 -R $CONDA_PREFIX/lib/python3.8/site-packages/mujoco_py
-    sudo apt update && sudo apt install libosmesa6-dev
+    sudo apt update && sudo apt install -y libosmesa6-dev libglew-dev
     cd data/ && $CONDA_PREFIX/bin/python download_d4rl_datasets.py
     cd ../
 }
 
-
-download_mujoco
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
