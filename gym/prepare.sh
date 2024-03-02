504c1ed1daaec0cd2b0716fa525dd84abe9b465a
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-03-02 03:29:37 +0000

    bad ordering of operations in prep

--- gym/prepare.sh
+++ gym/prepare.sh
@@ -7,20 +7,29 @@ install_conda() {
     rm -rf ~/miniconda3/miniconda.sh
 }
 
-wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
-tar -xf mujoco210-linux-x86_64.tar.gz 
-mkdir ~/.mujoco/
-mv mujoco210/ ~/.mujoco/mujoco210
-rm mujoco210-linux-x86_64.tar.gz 
+download_mujoco() {
+    wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
+    tar -xf mujoco210-linux-x86_64.tar.gz 
+    mkdir ~/.mujoco/
+    mv mujoco210/ ~/.mujoco/mujoco210
+    rm mujoco210-linux-x86_64.tar.gz 
+}
+
+prep_env() {
+    conda env create -f conda_env.yml
+
+    conda activate decision-transformer-gym
+    $CONDA_PREFIX/bin/pip3 install git+https://github.com/Farama-Foundation/d4rl@master#egg=d4rl
+}
+
+download_data() {
+    sudo chmod 777 -R $CONDA_PREFIX/lib/python3.8/site-packages/mujoco_py
+    sudo apt update && sudo apt install libosmesa6-dev
+    cd data/ && $CONDA_PREFIX/bin/python download_d4rl_datasets.py
+    cd ../
+}
+
+
+download_mujoco
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
-
-conda env create -f conda_env.yml
-
-conda activate decision-transformer-gym
-$CONDA_PREFIX/bin/pip3 install git+https://github.com/Farama-Foundation/d4rl@master#egg=d4rl
-sudo chmod 777 
-sudo apt update && sudo apt install libosmesa6-dev
-cd data/ && $CONDA_PREFIX/bin/python download_d4rl_datasets.py
-cd ../
-
