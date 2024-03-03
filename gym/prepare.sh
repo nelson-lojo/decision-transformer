cfb5a3639661918c67f0bd8d29e03e78224e332f
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-03-03 02:01:38 +0000

    forgot to do this 1 too many times

--- gym/prepare.sh
+++ gym/prepare.sh
@@ -25,6 +25,7 @@ prep_conda() {
 }
 
 prep_env() {
+    conda activate decision-transformer-gym
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 }
