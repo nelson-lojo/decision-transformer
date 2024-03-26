447d5ee5b929d8c9d6c922c5da5dbfe4605b8754
DerrickhSun <derrick.han.sun@gmail.com>
2024-03-26 22:06:20 +0000

    initial data-to-traj, needs testing, terminalvar still in-progress

--- gym/file_to_trajectory.py
+++ gym/file_to_trajectory.py
@@ -17,7 +17,48 @@ from decision_transformer.models.mlp_bc import MLPBCModel
 from decision_transformer.training.act_trainer import ActTrainer
 from decision_transformer.training.seq_trainer import SequenceTrainer
 
+# file_name is a pickle file containing a list of tuples (reward, observation, action) that are assumed sequential
+# data_to_trajectory will convert the file of tuples to a pickle file with a trajectory
+def data_to_trajectory(file_name):
+    
+    output = {}
+    
+    with open(f'{file_name}.pkl', 'rb') as f:
+        data = pickle.load(f)
+        # trajectories have: observations, next_observations, actions, rewards, terminals
+        observations = []
+        next_observations = []
+        actions = []
+        rewards = []
+        terminals = []
 
+        for i in range(len(data)-1):
+            observations.append(data[i][1])
+            next_observations.append(data[i + 1][1])
+            actions.append(data[i][2])
+            rewards.append(data[i][0])
+
+            # need to figure out how to get terminals
+            terminals.append(False)
+
+        output['observations'] = observations
+        output['next_observations'] = next_observations
+        output['actions'] = actions
+        output['rewards'] = rewards
+        output['terminals'] = terminals
+
+
+    with open(f'{name}.pkl', 'wb') as f:
+        pickle.dump(output, f)
+
+        
+    
+        
+
+        
+    
+    
+    
 
 
 
